cebf486ffcbad35430650ff993f9d881d8f20092
Jyotsanapandey <115559485+Jyotsanapandey@users.noreply.github.com>
2022-11-22 18:35:10 +0530

    game f

---
+++ functions/game_example1.py
@@ -0,0 +1,12 @@
+import pgzrun
+HEIGHT = 300
+WIDTH = 800
+p = Actor('ironman',(200,100))
+c = Actor('cookie_img') 
+
+def draw():
+    screen.fill('white')
+    p.draw()
+    c.draw()
+
+pgzrun.go()
