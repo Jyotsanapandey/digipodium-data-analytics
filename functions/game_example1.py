21db370f9d56448b33a429e9bcf828fe1b3c800d
Jyotsanapandey <115559485+Jyotsanapandey@users.noreply.github.com>
2022-11-24 18:38:15 +0530

    game function 2

--- functions/game_example1.py
+++ functions/game_example1.py
@@ -8,5 +8,21 @@ def draw():
     screen.fill('white')
     p.draw()
     c.draw()
+    print('drawing')
+
+def update():
+    print('updating')
+    p.x += 1
+    p.angle = -10
+    if p.x > WIDTH:
+        p.x = 0
+
+    p.x -= 3
+    p.angle = -10
+    if p.x < 0: # agar player left side se bahar jaye toh
+        p.x = WIDTH
+    print(p.x , p.y)        
+
+    print(p.x , p.y)
 
 pgzrun.go()
